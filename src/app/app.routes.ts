f3095202b96673367adc03649e2e536846f87e4d
Giulia <giulia.zecchetin@gmail.com>
2025-11-04 14:40:25 +0100

    Added Home component with routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { 
+    path: 'login', 
+    component: Login,
+    title: 'Login'
+  },
+  { 
+    path: '', 
+    redirectTo: 'login', 
+    pathMatch: 'full' 
+  },
+  {
+    path: '/home',
+    component: Home,
+    title: 'Home'
+  },
 ];
